48170027d6bef297bca3f7c8c9484a27c667099f
lukasz <lukol12321@gmail.com>
2024-12-06 23:02:40 +0100

    Added H2 db with hibernate

--- src/main/java/org/lukas/countries/controllers/CountryController.java
+++ src/main/java/org/lukas/countries/controllers/CountryController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CountryController {
     @GetMapping("/")
     public String test() {
-        return "eloelo";
+        return "";
     }
 }
